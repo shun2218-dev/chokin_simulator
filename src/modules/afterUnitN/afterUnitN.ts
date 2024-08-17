1389eec55dbe081a44d1b5342573819f568dca2d
shun2218-dev <kohaku1015game@gmail.com>
2024-08-17 13:07:10 +0900

    refactor: 💡 add JSDoc and comment

--- src/modules/afterUnitN/afterUnitN.ts
+++ src/modules/afterUnitN/afterUnitN.ts
@@ -1,7 +1,14 @@
 import { Unit } from '@/types';
 import dayjs from 'dayjs';
 
-export const afterUnitN = (n: number, unit: Unit, from = dayjs()) => {
+/**
+ * return a number of days which is 'from' 'n' 'unit' later
+ * @param {number} n a number of later('unit')
+ * @param {Unit} unit unit of 'n'
+ * @param {dayjs.Dayjs} [from=dayjs()] date of reference
+ * @returns {number} after a number of 'n' 'unit' days 'from'
+ */
+export const afterUnitN = (n: number, unit: Unit, from: dayjs.Dayjs = dayjs()): number => {
   const nLaterUnit = from.add(n, unit);
   return nLaterUnit.diff(from, 'day');
 };
