abdfac24c3878355e4a56f41edc99ba05fb84007
shun2218-dev <kohaku1015game@gmail.com>
2024-08-17 13:16:35 +0900

    refactor: 💡 add from of args, JSDoc and comment

--- src/modules/calculatePerMonth/calculatePerMonth.ts
+++ src/modules/calculatePerMonth/calculatePerMonth.ts
@@ -1,8 +1,18 @@
 import dayjs from 'dayjs';
 
-export const calculatePerMonth = (duration: number, targetAmount: number): number => {
+/**
+ * return amount of money per Month on a daily basis
+ * @param {number} duration a number of days duration
+ * @param {number} targetAmount the target which is amount of money
+ * @param {dayjs.Dayjs} [from=dayjs()] date of reference
+ * @returns amount of money per Month on a daily basis
+ */
+export const calculatePerMonth = (
+  duration: number,
+  targetAmount: number,
+  from: dayjs.Dayjs = dayjs(),
+): number => {
   const perDay = targetAmount / duration;
-  const today = dayjs();
-  const oneMonthLater = today.add(1, 'month');
-  return Math.round(perDay * oneMonthLater.diff(today, 'day') * 100) / 100;
+  const oneMonthLater = from.add(1, 'month');
+  return Math.round(perDay * oneMonthLater.diff(from, 'day') * 100) / 100;
 };
