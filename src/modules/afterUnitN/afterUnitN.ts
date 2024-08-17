a74d33508c91d637b3cab8b0b09897152d9a7d61
shun2218-dev <kohaku1015game@gmail.com>
2024-08-17 12:57:01 +0900

    refactor: 💡 add from of args

--- src/modules/afterUnitN/afterUnitN.ts
+++ src/modules/afterUnitN/afterUnitN.ts
@@ -1,8 +1,7 @@
 import { Unit } from '@/types';
 import dayjs from 'dayjs';
 
-export const afterUnitN = (n: number, unit: Unit) => {
-  const today = dayjs();
-  const nLaterUnit = today.add(n, unit);
-  return nLaterUnit.diff(today, 'day');
+export const afterUnitN = (n: number, unit: Unit, from = dayjs()) => {
+  const nLaterUnit = from.add(n, unit);
+  return nLaterUnit.diff(from, 'day');
 };
